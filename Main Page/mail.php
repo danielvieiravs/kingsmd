53676d044db3b6691424dffcd31e1d0a173105a4
Fabio <fabiovieira79@hotmail.com>
2018-12-04 15:21:11 +0000

    email

--- Main Page/mail.php
+++ Main Page/mail.php
@@ -1,56 +1,27 @@
 <?php
 
-  $receiver = strip_tags($_POST['receiver']);
-  $name = strip_tags($_POST['name']);
-  $email = strip_tags($_POST['email']);
-  $phone = strip_tags($_POST['phone']);
-  $service = strip_tags($_POST['service']);
-  $car = strip_tags($_POST['car']);
-  $date = strip_tags($_POST['date']);
-  $time = strip_tags($_POST['time']);
-  $message = strip_tags($_POST['message']);
-
+  $receptor = $_POST['receptor'];
+  $email = $_POST['email'];
+  $data = $_POST['data'];
   $subject = 'Kings Movement & Detailing - '.$email;
 
-  $content =
-  '<html>
-    <body>
-      <table style="padding: 20px; border-collapse: collapse;">
-        <tr style="font-size: 15px;">
-          <td style="font-weight: bold; padding: 10px 25px 10px 0;">Name</td>
-          <td>'.$name.'</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold; padding: 10px 25px 10px 0;">Email</td>
-          <td>'.$email.'</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold; padding: 10px 25px 10px 0;">Phone</td>
-          <td>'.$phone.'</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold; padding: 10px 25px 10px 0;">Car</td>
-          <td>'.$car.'</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold; padding: 10px 25px 10px 0;">Service</td>
-          <td>'.$service.'</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold; padding: 10px 25px 10px 0;">Date</td>
-          <td>'.$date.'</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold; padding: 10px 25px 10px 0;">Time</td>
-          <td>'.$time.'</td>
-        </tr>
-        <tr>
-          <td style="font-weight: bold; padding: 10px 25px 10px 0;">Message</td>
-          <td>'.$message.'</td>
-        </tr>
-      </table>
-    </body>
-  </html>';
+  $content = '<html><body><table style="padding: 20px; border-collapse: collapse;">';
+
+  foreach ($data as $key => $value) {
+    $content .=
+    '<tr style="font-size: 15px;">
+      <td style="font-weight: bold; padding: 15px 25px 10px 0;">'.$key.'</td>
+    </tr>';
+
+    foreach ($value as $data) {
+      $content .=
+      '<tr style="font-size: 15px;">
+        <td style="font-weight: bold; padding: 5px 25px;">'.$data['prop'].'</td>
+        <td>'.$data['value'].'</td>
+      </tr>';
+    }
+  }
+  $content .= '</table></body></html>';
 
   $headers = "From: " . $email . "\r\n";
   $headers .= "Reply-To: " . $email . "\r\n";
@@ -60,10 +31,10 @@
   $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
   $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-  if (mail($receiver, $subject, $content, $headers)) {
-    echo 'send';
-  } else {
-    echo 'reject';
+  if (mail($receptor, $subject, $content, $headers)) {
+    echo 'ok';
+  }else{
+    echo 'error';
   }
 
 ?>
